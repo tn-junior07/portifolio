990b48d93baebcfb89e2a236be92063b8b81da45
tn-junior07 <tn-junior@hotmail.com>
2024-06-21 15:15:11 -0300

    Up EducationCard.jsx

--- src/components/Cards/EducationCard.jsx
+++ src/components/Cards/EducationCard.jsx
@@ -7,7 +7,7 @@ const Document = styled.img`
     width: fit-content;
     background-color: #000;
     border-radius: 10px;
-    &:hover{
+    &:hover {
         cursor: pointer;
         opacity: 0.8;
     }
@@ -19,18 +19,18 @@ const Description = styled.div`
     font-weight: 400;
     color: ${({ theme }) => theme.text_primary + 99};
     margin-bottom: 10px;
-    @media only screen and (max-width: 768px){
+    @media only screen and (max-width: 768px) {
         font-size: 12px;
     }
 `
 
 const Span = styled.span`
-overflow: hidden;
-display: -webkit-box;
-max-width: 100%;
--webkit-line-clamp: 4;
--webkit-box-orient: vertical;
-text-overflow: ellipsis;
+    overflow: hidden;
+    display: -webkit-box;
+    max-width: 100%;
+    -webkit-line-clamp: 4;
+    -webkit-box-orient: vertical;
+    text-overflow: ellipsis;
 `
 
 const Card = styled.div`
@@ -45,34 +45,32 @@ const Card = styled.div`
     flex-direction: column;
     gap: 12px;
     transition: all 0.3s ease-in-out;
-    &:hover{
-        box-shadow: 0px 0px 20px rgba(0,0,0,0.2);
+    &:hover {
+        box-shadow: 0px 0px 20px rgba(0, 0, 0, 0.2);
         transform: translateY(-5px);
     }
-    @media only screen and (max-width: 768px){
+    @media only screen and (max-width: 768px) {
         padding: 10px;
         gap: 8px;
         width: 300px;
     }
 
-    &:hover ${Document}{
+    &:hover ${Document} {
         display: flex;
     }
 
-    &:hover ${Span}{
+    &:hover ${Span} {
         overflow: visible;
         -webkit-line-clamp: unset;
-
     }
-    border: 0.1px solid #854CE6;
+    border: 0.1px solid #854ce6;
 `
 
 const Top = styled.div`
     width: 100%;
     display: flex;
-    gap: 12px
+    gap: 12px;
 `
-
 const Image = styled.img`
     height: 50px;
     background-color: #000;
@@ -86,15 +84,14 @@ const Image = styled.img`
 const Body = styled.div`
     width: 100%;
     display: flex;
-    flex-direction: column; 
+    flex-direction: column;
 `
-
 
 const Name = styled.div`
     font-size: 18px;
     font-weight: 600;
     color: ${({ theme }) => theme.text_primary + 99};
-    @media only screen and (max-width: 768px){
+    @media only screen and (max-width: 768px) {
         font-size: 14px;
     }
 `
@@ -103,7 +100,7 @@ const Degree = styled.div`
     font-size: 14px;
     font-weight: 500;
     color: ${({ theme }) => theme.text_secondary + 99};
-    @media only screen and (max-width: 768px){
+    @media only screen and (max-width: 768px) {
         font-size: 12px;
     }
 `
@@ -112,34 +109,22 @@ const Date = styled.div`
     font-size: 12px;
     font-weight: 400;
     color: ${({ theme }) => theme.text_secondary + 80};
-    @media only screen and (max-width: 768px){
+    @media only screen and (max-width: 768px) {
         font-size: 10px;
     }
 `
-
-const Grade = styled.div`
-    font-size: 14px;
-    font-weight: 500;
-    color: ${({ theme }) => theme.text_secondary + 99};
-    @media only screen and (max-width: 768px){
-        font-size: 12px;
-    }
-`
-
-
 
 const EducationCard = ({ education }) => {
     return (
         <Card>
             <Top>
-                <Image src={education.img} />
+            <Image src={education.img} />
                 <Body>
                     <Name>{education.school}</Name>
                     <Degree>{education.degree}</Degree>
                     <Date>{education.date}</Date>
                 </Body>
             </Top>
-            <Grade><b>Grade: </b>{education.grade}</Grade>
             <Description>
                 <Span>{education.desc}</Span>
             </Description>
